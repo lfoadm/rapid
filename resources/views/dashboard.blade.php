af38bf5ab7299cb4d279341881efc35638c1d7f2
Leandro <lfoadm@icloud.com>
2024-07-08 22:11:30 -0300

    Carol

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,7 @@
                     {{ __("You're logged in!") }}
                     @can('is-admin')
                     <h1 class="text-7xl">SÓ ADMIN QUEM VE!!</h1>
+                    <h1 class="text-pink-500 mt-10 text-7xl">CAROLINA!!</h1>
                     @endcan
                 </div>
             </div>
